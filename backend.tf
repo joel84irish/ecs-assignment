65b3757e5600a5376fc022f0a4735416c7a17ba2
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-20 17:56:17 +0000

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_version = ">=1.10.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.80.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  
+}
+
+
+# S3 Bucket for TF State File
+resource "aws_s3_bucket" "terraform_state" {
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Dynamo DB Table for Locking TF Config
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
