05b026e22065a3254d97108439af5a5147fe5b60
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-20 20:11:40 +0000

    Update varibales.tf

--- varibales.tf
+++ varibales.tf
@@ -9,7 +9,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "AZs for Subnets"
   type        = list(string)
-  default     = ["us-wast-2a", "us-west-2b"]
+  default     = ["us-west-2a", "us-west-2b", "us-west-2b"]
 }
 
 variable "public_subnet_cidrs" {
