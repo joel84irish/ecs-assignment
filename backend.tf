1e65934434b3063719d392585c709dcf162f1ce4
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-20 19:53:08 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,4 @@
-terraform {
+l-terraform {
   required_version = ">=1.0.1"
   required_providers {
     aws = {
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 
 # Dynamo DB Table for Locking TF Config
 resource "aws_dynamodb_table" "terraform_lock1" {
-  name         = "terraform-state-locking-project"
+  name         = "Joel-terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
