ec7b45d0639c0f89f3ffe7ddee897d85c6c79dfd
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-27 12:10:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,30 @@
-resource "aws_vpc" "my-vpc" {
+resource "aws_vpc" "my-vpc-tf" {
  cidr_block = var.vpc_cidr
 }
 
 resource "aws_subnet" "sn1" {
-  vpc_id                  = aws_vpc.my-vpc.id
+  vpc_id                  = aws_vpc.my-vpc-tf.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = var.availability_zones[0]
   map_public_ip_on_launch = true
+  tags = {
+    Name    = "PublicSubnet-tf"
+  }
 }
 
 resource "aws_subnet" "sn2" {
-  vpc_id                  = aws_vpc.my-vpc.id
+  vpc_id                  = aws_vpc.my-vpc-tf.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = var.availability_zones[1]
   map_public_ip_on_launch = true
-}
-#
-resource "aws_subnet" "sn3" {
-  vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = var.private_subnet_cidrs[0]
-  availability_zone = var.availability_zones[1]
   tags = {
-    Name    = "PrivateSubnet"
+    Name    = "PublicSubnet2-tf"
   }
 }
 
 
-
-resource "aws_security_group" "sg" {
-  name        = "sg"
+resource "aws_security_group" "sg-tf" {
+  name        = "sg-tf"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.my-vpc.id
 
@@ -39,29 +35,32 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "sg2" {
 
   ingress {
-    description      = "htts"
+    description      = "https"
     from_port        = 3000
     to_port          = 3000
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
-
-
-  egress {
+  
+egress {
     from_port       = 0
     to_port         = 0
-    protocol        = "-1"
+    protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_internet_gateway" "gw-tf" {
+  vpc_id = aws_vpc.my-vpc-tf.id
 }
 
 resource "aws_route_table" "rt" {
@@ -69,12 +68,12 @@ resource "aws_route_table" "rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.gw-tf.id
   }
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
+    gateway_id      = aws_internet_gateway.gw-tf.id
   }
 }
 
@@ -88,8 +87,8 @@ resource "aws_route_table_association" "route2" {
   subnet_id      = aws_subnet.sn2.id
 }
 
-resource "aws_network_interface" "ecs_eni" {
+resource "aws_network_interface" "ecs_eni-tf" {
   subnet_id = aws_subnet.sn1.id
-  security_groups = [aws_security_group.sg.id]
+  security_groups = [aws_security_group.sg-tf.id]
 }
 
