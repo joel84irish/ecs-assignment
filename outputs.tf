94e004a056b1ca443178f07303dcf3766fd27f1f
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-27 21:15:13 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "dns_name" {
   description = "The DNS name of the created ELB."
-  value = aws_elb.aws_alb.application_load_balancer.dns_name
+  value = aws_alb.application_load_balancer.dns_name
 }    
