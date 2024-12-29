f76fdc51352e1f1769f269ef854cf26b6e28354f
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-29 01:03:32 +0000

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,8 +1,46 @@
+resource "aws_ecs_cluster" "ecs" {
+ name = "TERRAFORM_CLUSTER"
+}
+
+
+resource "aws_ecs_task_definition" "ECS_TASK_DEFINITION_APP:5" {
+  container_definitions = jsonencode([
+   {
+     name      = "app"
+     image     = "766261352911.dkr.ecr.us-west-2.amazonaws.com/app"
+     cpu       = 2048
+     memory    = 4096
+     essential = true
+     portMappings = [
+       {
+         containerPort = 3000
+         hostPort      = 3000
+         protocol      = "tcp"
+       }
+     ]
+   }
+ ])
+
+ family                   = "app"
+ requires_compatibilities = ["FARGATE"]
+
+ cpu                      = "2048"
+ memory                   = "4096"
+
+ network_mode             = "awsvpc"
+ task_role_arn            = "arn:aws:iam::766261352911:role/ecsTaskExecutionRole"
+ execution_role_arn       = "arn:aws:iam::766261352911:role/ecsTaskExecutionRole"
+ 
+ runtime_platform {
+   operating_system_family = "LINUX"
+   cpu_architecture        = "ARM64"
+ } 
+}
 
 resource "aws_ecs_service" "SERVICE_APP_FINAL" {
   name = "TERRAFORM_SERVICE"
   cluster                = aws_ecs_cluster.TERRAFORM_CLUSTER.arn
-  task_definition        = aws_ecs_task_definition.ECS_TASK_DEFINITION_APP.arn  
+  task_definition        = aws_ecs_task_definition.ECS_TASK_DEFINITION_APP:5.arn  
   launch_type            = "FARGATE"
   desired_count          = 2  
   force_new_deployment   = true  
